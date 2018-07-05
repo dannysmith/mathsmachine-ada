8060571801f2471038e24d2eab7eb29e8fe9fe7e
Danny Smith <hi@danny.is>
2018-07-05 12:10:37 +0100

    Add test for Array#push

--- test/mathsmachine.test.js
+++ test/mathsmachine.test.js
@@ -1,2 +1,10 @@
 const chai = require('chai');
 const expect = chai.expect;
+
+describe('#push()', function () {
+  it('should add an item to the array', function () {
+    array = [1, 2, 3, 4]
+    array.push(6)
+    expect(array).to.eql([1, 2, 3, 4, 6]);
+  });
+});
